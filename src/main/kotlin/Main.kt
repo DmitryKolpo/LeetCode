9ecba578b9287e8bc1f01000b655d05240e0ce14
d.sheverin <d.kolponosov@3commas.io>
2023-07-12 16:55:17 +0300

    implement Add two sum

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    val solution = Solution()
+    val testCases = listOf(
+        (intArrayOf(2, 7, 11, 15) to 9) to intArrayOf(0, 1),
+        (intArrayOf(3, 2, 4) to 6) to intArrayOf(1, 2),
+        (intArrayOf(2, 7, 11, 15) to 9) to intArrayOf(0, 1),
+        (intArrayOf(Int.MAX_VALUE, 1, 2) to 3) to intArrayOf(1, 2),
+        (intArrayOf(Int.MAX_VALUE, Int.MIN_VALUE, 1, 2) to 3) to intArrayOf(2, 3),
+        (intArrayOf(Int.MAX_VALUE, Int.MIN_VALUE, 1, 2) to -1) to intArrayOf(0, 1),
+        (intArrayOf(Int.MAX_VALUE, Int.MAX_VALUE, 0, -2) to -2) to intArrayOf(2, 3),
+    )
+
+    testCases.forEach {
+        val input = it.first
+        val actual = solution.twoSum(input.first, input.second)
+        val expected = it.second
+        println("actual=${actual.toList()}; expected=${expected.toList()}")
+    }
+}
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        for (i in 0 until nums.lastIndex) {
+            for (j in i + 1..nums.lastIndex) {
+                val firstNumber = nums[i].toLong()
+                val secondNumber = nums[j].toLong()
+                val success = firstNumber + secondNumber == target.toLong()
+                if (success) return intArrayOf(i, j)
+            }
+        }
+        throw Exception("There is no solution.")
+    }
+}
+
